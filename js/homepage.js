6489bfc44f2f4eb467fbba6ab60868c1a6917b5c
jaineRodrigues <jainer469@gmail.com>
2023-04-04 16:00:46 -0300

    Adicionando homepage pública

---
+++ js/homepage.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+
