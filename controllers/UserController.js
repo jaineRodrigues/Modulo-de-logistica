f0b8f6e06647ebef684d07a264151f8a00c047d7
LucasRibeiro36 <lucasribeiro5569@gmail.com>
2023-04-19 12:24:29 -0300

    Adicionando handlebars e js

---
+++ controllers/UserController.js
@@ -0,0 +1,22 @@
+
+/*controllers/UserController.js
+const showCreateForm = (req, res) => {
+    res.render('/login');
+  };
+  
+  module.exports = {
+    showCreateForm,
+  };
+  
+  // controllers/UserController.js
+const createUser = (req, res) => {
+    const { nome, email, senha } = req.body;
+    // Lógica para salvar usuário no banco de dados
+    res.redirect('/usuarios');
+  };
+  
+  module.exports = {
+    showCreateForm,
+    createUser,
+  };
+  */
