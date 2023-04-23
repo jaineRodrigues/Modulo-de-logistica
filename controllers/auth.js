cdebc33aeffba727f64b6e82f4098cebdf2ef5a9
LucasRibeiro36 <lucasribeiro5569@gmail.com>
2023-04-22 23:18:46 -0300

    add exception e iniciando npm

--- controllers/auth.js
+++ controllers/auth.js
@@ -77,35 +77,42 @@ exports.login = async (req, res) => {
           });
         }
 
-        if (!results || results.length === 0 || !(await bcrypt.compare(password, results[0].password))) {
-          res.status(401).render('login', {
-            message: "Email or Password is incorrect",
+        try {
+          if (!results || results.length === 0 || !(await bcrypt.compare(password, results[0].password))) {
+            res.status(401).render('login', {
+              message: "Email or Password is incorrect",
+            });
+  
+          } else if (
+            !results ||
+            !(await bcrypt.compare(password, results[0].password))
+          ) {
+            res.status(401).render('login', {
+              message: "Email or Password is incorrent",
+            });
+          } else {
+            const id = results[0].id;
+  
+            const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+              expiresIn: process.env.JWT_EXPIRES_IN,
+            });
+            console.log("The token is: " + token);
+  
+            const cookieOptions = {
+              expires: new Date(
+                Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+              ),
+              httpOnly: true,
+            };
+  
+            res.cookie("jwt", token, cookieOptions);
+            res.status(200).redirect("/");
+          }
+        } catch (error) {
+          console.log(error);
+          return res.status(500).render("login", {
+            message: "Internal server error",
           });
-
-        } else if (
-          !results ||
-          !(await bcrypt.compare(password, results[0].password))
-        ) {
-          res.status(401).render('login', {
-            message: "Email or Password is incorrent",
-          });
-        } else {
-          const id = results[0].id;
-
-          const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-            expiresIn: process.env.JWT_EXPIRES_IN,
-          });
-          console.log("The token is: " + token);
-
-          const cookieOptions = {
-            expires: new Date(
-              Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
-            ),
-            httpOnly: true,
-          };
-
-          res.cookie("jwt", token, cookieOptions);
-          res.status(200).redirect("/");
         }
       }
     );
