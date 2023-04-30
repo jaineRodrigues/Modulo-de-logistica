769bb93d8a402f4d7c8a6c5533b98108f937047f
jaineRodrigues <jainer469@gmail.com>
2023-04-30 14:18:58 -0300

    Configurando model de usuario

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const User = sequelize.define('usuarios', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  userType: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  timestamps: false // desativa as colunas createdAt e updatedAt
+});
+
+module.exports = User;
