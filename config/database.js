d317a5c54baedb6a746da57995da2e352bc7d77b
jaineRodrigues <jainer469@gmail.com>
2023-04-30 14:16:50 -0300

    Configurando arquivo de conexão

---
+++ config/database.js
@@ -0,0 +1,23 @@
+const { Sequelize } = require('sequelize');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME,
+  process.env.DATABASE_USER,
+  process.env.DATABASE_PASSWORD, {
+  host: process.env.DATABASE_HOST,
+  dialect: process.env.DATABASE_DIALECT || 'mysql', // fornecer um dialeto padrão caso nenhum seja fornecido
+});
+
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Conexão com o banco de dados estabelecida com sucesso.');
+  } catch (error) {
+    console.error('Não foi possível se conectar ao banco de dados:', error);
+  }
+})();
+
+module.exports = sequelize;
