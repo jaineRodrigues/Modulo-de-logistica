170d4972e7f96b358a96561acba586794b98b3c0
jaineRodrigues <jainer469@gmail.com>
2023-06-01 18:07:11 -0300

    Configurado rota get

--- routes/pages.js
+++ routes/pages.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { authMiddleware } = require('../middlewares/authMiddleware');
 const { listVehicles } = require('../middlewares/listVehicles');
+const { listCenters } = require('../middlewares/centerMiddleware');
 
 router.get('/', (req, res) => {
     res.render('index', { title: 'Página Inicial' });
@@ -17,31 +18,32 @@ router.get('/login', (req, res) => {
 
 router.get('/clientPanel', authMiddleware, (req, res) => {
     res.render('clientPanel', {
-    title: 'Painel do Cliente' ,
-    name: res.locals.user.name,
-    email: res.locals.user.email,
-    telefone: res.locals.user.telefone,
-    cpf: res.locals.user.cpf
-});
+        title: 'Painel do Cliente',
+        name: res.locals.user.name,
+        email: res.locals.user.email,
+        telefone: res.locals.user.telefone,
+        cpf: res.locals.user.cpf
+    });
 })
 
 router.get('/adminPanel', authMiddleware, (req, res) => {
-    res.render('adminPanel', { title: 'Painel do Administrador' ,
-    name: res.locals.user.name,
-});
+    res.render('adminPanel', {
+        title: 'Painel do Administrador',
+        name: res.locals.user.name,
+    });
 })
 
-router.get('/cadveiculos',authMiddleware,  (req, res) => {
-    res.render('cadveiculos', { 
+router.get('/cadveiculos', authMiddleware, (req, res) => {
+    res.render('cadveiculos', {
         title: 'Cadastrar Veículos',
     });
 })
 
-router.get('/cadcondutores',authMiddleware, (req, res) => {
+router.get('/cadcondutores', authMiddleware, (req, res) => {
     res.render('cadcondutores', { title: 'Cadastrar Condutores' });
 })
 
-router.get('/cadtransportadora',authMiddleware, (req, res) => {
+router.get('/cadtransportadora', authMiddleware, (req, res) => {
     res.render('cadtransportadora', { title: 'Cadastrar Transportadora' });
 })
 
@@ -55,20 +57,47 @@ router.get('/personalData', authMiddleware, (req, res) => {
     });
 });
 
-router.get('/changePassword', (req,res) =>{
-    res.render('changePassword', { title: 'Recuperar senha'});
+router.get('/changePassword', (req, res) => {
+    res.render('changePassword', { title: 'Recuperar senha' });
 })
 
 router.get('/serviceRegistration', listVehicles, (req, res) => {
-    res.render('serviceRegistration', { 
+    res.render('serviceRegistration', {
         title: 'Registro de Serviços',
         vehicles: res.locals.vehicles,
     });
 });
 
-router.get('/logout',authMiddleware, (req, res) => {
+router.get('/logout', authMiddleware, (req, res) => {
     res.cookie("jwt", "", { maxAge: 1 });
     res.redirect("/login");
 })
+
+router.get('/centerRegistration', (req, res) => {
+    res.render('centerRegistration', {
+        title: 'Registro de Centros de Distribuição',
+    });
+});
+
+router.get('/distribuitionCenter', (req, res) => {
+    res.render('distribuitionCenter', {
+        title: 'Centros de Distribuição',
+    });
+});
+
+router.get('/editCenter/:id/edit', listCenters, (req, res) => {
+    res.render('editCenter', {
+        title: 'Editar Centros de Distribuição',
+        id: req.params.id,
+        centers: res.locals.centers,
+    });
+});
+
+router.get('/centerList', listCenters, (req, res) => {
+    res.render('centerList', {
+        title: 'Listar Centros de Distribuição',
+        centers: res.locals.centers,
+    });
+});
 
 module.exports = router;
